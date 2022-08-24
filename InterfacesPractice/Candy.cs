481e0ee9e2325223c2b113fdea13514294fc9764
MattColaProgrammer93 <mattchoque115@gmail.com>
2022-08-24 13:03:19 -0700

    Created the Candy class

---
+++ InterfacesPractice/Candy.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfacesPractice
+{
+    /// <summary>
+    /// Represents as a single piece of candy
+    /// </summary>
+    public class Candy
+    {
+        /// <summary>
+        /// The candy's name
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// The candy's flavor
+        /// </summary>
+        public string Flavor { get; set; }
+
+        /// <summary>
+        /// The date of the candy created
+        /// </summary>
+        public DateTime DateOfCreation { get; set; }
+    }
+}
